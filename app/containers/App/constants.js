da1ad6e8f7161701e6d7b7faaf45caa9ee7f4ec6
Hatimtech <46959498+Hatimtech@users.noreply.github.com>
2020-03-18 10:58:20 +0530

    Create constants.js

---
+++ app/containers/App/constants.js
@@ -0,0 +1,34 @@
+export const DEFAULT_LOCALE = 'en';
+// export const API_URL = 'http://localhost:3001/api';
+// export const STATIC_URL = 'http://localhost:3001/uploads/';
+
+// export const API_URL = 'http://10.182.23.24:3001/api';
+// export const STATIC_URL = 'http://10.182.23.24:3001/uploads/';
+
+//PRODUCTION
+// export const API_URL = 'http://35.204.144.169:3001/api';
+// export const STATIC_URL = 'http://35.204.144.169:3001/uploads/';
+
+//testing
+// export const API_URL = 'http://35.226.109.150:3001/api';
+// export const STATIC_URL = 'http://35.226.109.150:3001/uploads/';
+
+//Develoment
+// export const API_URL = 'http://localhost:3001/api';
+// export const STATIC_URL = 'http://localhost:3001/uploads/';
+
+//Develoment
+export const API_URL = 'http://139.59.13.124:3001/api';
+export const STATIC_URL = 'http://139.59.13.124:3001/uploads/';
+
+export const CONTRACT_URL = 'http://35.239.63.105/ipfs/';
+export const CURRENCY = 'XOF';
+export const CHANGE_USERNAME = 'CHANGE_USERNAME';
+export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
+export const SET_AUTH = 'SET_AUTH';
+export const SENDING_REQUEST = 'SENDING_REQUEST';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const REGISTER_REQUEST = 'REGISTER_REQUEST';
+export const LOGOUT = 'LOGOUT';
+export const REQUEST_ERROR = 'REQUEST_ERROR';
+export const CLEAR_ERROR = 'CLEAR_ERROR';
