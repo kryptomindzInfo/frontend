01f6d34c7a1c89cbc6976d9e2dbbf82fdfeed424
beyondewallet <58459451+beyondewallet@users.noreply.github.com>
2020-03-18 12:30:23 +0530

    update k8s server domain

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -18,10 +18,26 @@ export const DEFAULT_LOCALE = 'en';
 // export const STATIC_URL = 'http://localhost:3001/uploads/';
 
 //Develoment
-export const API_URL = 'http://139.59.13.124:3001/api';
-export const STATIC_URL = 'http://139.59.13.124:3001/uploads/';
+// export const API_URL = 'http://139.59.13.124:3001/api';
+// export const STATIC_URL = 'http://139.59.13.124:3001/uploads/';
 
-export const CONTRACT_URL = 'http://35.239.63.105/ipfs/';
+// export const CONTRACT_URL = 'http://35.239.63.105/ipfs/';
+
+// export const API_URL = 'http://localhost:3001/api';
+// export const STATIC_URL = 'http://localhost:3001/uploads/';
+
+// export const API_URL = 'http://35.204.144.169:3001/api';
+// export const STATIC_URL = 'http://35.204.144.169:3001/uploads/';
+
+//export const CONTRACT_URL = 'http://34.70.46.65:8080/ipfs/';
+
+// production config k8s
+export const SERVER_URL = '91d90ac373dc.sn.mynetname.net:2020';
+export const API_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/api';
+export const STATIC_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/uploads/';
+export const CONTRACT_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/ipfs/';
+// end production config
+
 export const CURRENCY = 'XOF';
 export const CHANGE_USERNAME = 'CHANGE_USERNAME';
 export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
