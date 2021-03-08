995d7530961ebdc0fd976bb697755f1601a98c01
nischal <nischal@kmindz.in>
2021-03-08 10:51:15 +0530

    fixedbug

--- app/components/Header/BankHeader.js
+++ app/components/Header/BankHeader.js
@@ -64,7 +64,7 @@ class BankHeader extends Component {
                 <Link>
               Back
                 </Link>
-            </A>
+            </A>)
               :
               null
           }
